0f7b7af37e058984cbbdb3211a99afb5af00d2c7
Anh Phan <anhphanvt2705@gmail.com>
2023-07-12 10:43:16 +0700

    added detailed comments

--- image_stitch_wtih_crop_source.py
+++ image_stitch_wtih_crop_source.py
@@ -2,22 +2,48 @@ import cv2
 import glob
 
 def show_image(header, image):
+    """
+    Displays an image in a window with the specified header.
+    
+    Args:
+        header (str): Header for the image window.
+        image (numpy.ndarray): Image array to be displayed.
+    """
     print("[Console] Showing image")
     cv2.imshow(header, image)
     cv2.waitKey()
     
 def write_image(directory, image):
+    """
+    Saves an image to the specified directory.
+    
+    Args:
+        directory (str): Path to the directory where the image will be saved.
+        image (numpy.ndarray): Image array to be saved.
+    """
     print("[Console] Saving image")
     cv2.imwrite(directory, image)
 
 def get_images(directory):
+    """
+    Loads images from the specified directory.
+    
+    Args:
+        directory (str): Path to the directory containing the images.
+        
+    Returns:
+        List of loaded images (list of numpy.ndarray).
+        
+    Raises:
+        Exception: If the directory is invalid or no images are found.
+    """
     print("[Console] Accessing folder")
     image_paths = glob.glob(directory)
     print(image_paths)
     if len(image_paths) == 0:
         raise Exception("[Console] Invalid directory")
     images = []
-    # Add image to memory
+    # Add images to memory
     print("[Console] Loading Images")
     for image_path in image_paths:
         image = cv2.imread(image_path)
@@ -26,28 +52,76 @@ def get_images(directory):
     return images
 
 def get_gray_image(image):
+    """
+    Converts an RGB image to grayscale.
+    
+    Args:
+        image (numpy.ndarray): RGB image array.
+        
+    Returns:
+        Grayscale image (numpy.ndarray).
+    """
     return cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
 def get_stitch_image(image_list):
+    """
+    Stitches a list of images together to create a panoramic image.
+    
+    Args:
+        image_list (list): List of images to be stitched.
+        
+    Returns:
+        Stitched image (numpy.ndarray).
+        
+    Raises:
+        Exception: If stitching fails.
+    """
     print("[Console] Stitching Images")
-    # Stitch
+    # Stitch images
     stitcher = cv2.Stitcher.create()
     stitch_status, stitched_image = stitcher.stitch(image_list)
-    # Output
+    # Check stitching status
     if stitch_status == 0:
-        print("[Console] Stitch successfully")
+        print("[Console] Stitch successful")
         return stitched_image
     else:
         raise Exception("[Console] Stitch failed")
-    
+
 def get_threshold_image(gray_image):
+    """
+    Applies thresholding to a grayscale image.
+    
+    Args:
+        gray_image (numpy.ndarray): Grayscale image array.
+        
+    Returns:
+        Thresholded image (numpy.ndarray).
+    """
     print("[Console] Thresholding image")
     return cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY)[1]
 
 def get_image_2D_dim(image):
+    """
+    Returns the dimensions (height and width) of an image.
+    
+    Args:
+        image (numpy.ndarray): Image array.
+        
+    Returns:
+        Tuple containing the height and width of the image.
+    """
     return image.shape[:2]
 
 def get_mask_image(image):
+    """
+    Creates a mask image by converting the input image to grayscale, applying thresholding, and blurring.
+    
+    Args:
+        image (numpy.ndarray): Image array.
+        
+    Returns:
+        Mask image (numpy.ndarray).
+    """
     print("[Console] Masking image")
     gray_image = get_gray_image(image)
     # Threshold + Blur + Threshold = Remove all the random black pixel in the white part of the first threshold
@@ -57,6 +131,17 @@ def get_mask_image(image):
     return threshold_image
 
 def crop_image(image, factor):
+    """
+    Crops an image inward proportionally based on the specified factor.
+    
+    Args:
+        image (numpy.ndarray): Image array to be cropped.
+        factor (float): Proportion of the image to be cropped (0.0 to 1.0).
+        
+    Returns:
+        Tuple containing the crop location (lower height, upper height, left width, right width)
+        and the cropped image (numpy.ndarray).
+    """
     (h, w) = get_image_2D_dim(image)
     # Crop horizontally (width)
     amount_crop = w * (1-factor)
@@ -69,36 +154,77 @@ def crop_image(image, factor):
     return (h_lower, h_upper, w_left, w_right), image[h_lower:h_upper, w_left:w_right]
 
 def is_black_ver_line(image, start_h, end_h, w):
-    # Check if there is a black pixel in a straight vertical line
+    """
+    Checks if there is a black pixel in a straight vertical line within the specified image region.
+    
+    Args:
+        image (numpy.ndarray): Image array.
+        start_h (int): Starting height of the region.
+        end_h (int): Ending height of the region.
+        w (int): Width of the vertical line.
+        
+    Returns:
+        True if a black pixel is found, False otherwise.
+    """
     for value in range(start_h, end_h):
         if all(image[value, w] == [0, 0, 0]):
             return True
     return False
 
 def is_black_hor_line(image, start_w, end_w, h):
-    # Check if there is a black pixel in a straight horizontal line
+    """
+    Checks if there is a black pixel in a straight horizontal line within the specified image region.
+    
+    Args:
+        image (numpy.ndarray): Image array.
+        start_w (int): Starting width of the region.
+        end_w (int): Ending width of the region.
+        h (int): Height of the horizontal line.
+        
+    Returns:
+        True if a black pixel is found, False otherwise.
+    """
     for value in range(start_w, end_w):
         if all(image[h, value] == [0, 0, 0]):
             return True
     return False
         
 def is_black_pixel_outline(threshold_image):
-    # Find if there is black pixel on 4 sides
+    """
+    Checks if there are black pixels on the four sides of the thresholded image.
+    
+    Args:
+        threshold_image (numpy.ndarray): Thresholded image array.
+        
+    Returns:
+        True if black pixels are found on the outline, False otherwise.
+    """
     (height, width) = get_image_2D_dim(threshold_image)
     # Lower side (0, w)
-    if is_black_hor_line(threshold_image, 0, width, 0): return True
+    if is_black_hor_line(threshold_image, 0, width, 0):
+        return True
     # Upper side (h, w)
-    if is_black_hor_line(threshold_image, 0, width, height-1): return True
+    if is_black_hor_line(threshold_image, 0, width, height-1):
+        return True
     # Left side (h, 0)
-    if is_black_ver_line(threshold_image, 0, height, 0): return True
+    if is_black_ver_line(threshold_image, 0, height, 0):
+        return True
     # Right side (h, w)
-    if is_black_ver_line(threshold_image, 0, height, width-1): return True
+    if is_black_ver_line(threshold_image, 0, height, width-1):
+        return True
     return False
 
 def expand_from_crop_image(image, crop_location):
-    # Assuming it is proportionally cropped, expand each side until it hit a black pixel
-    # This will get the most image info in the expense of original image ratio
-    # w +- 5 instead of 1 for any error margin it may have
+    """
+    Expands the cropped image by searching for the nearest black pixels on each side.
+    
+    Args:
+        image (numpy.ndarray): Image array to be expanded.
+        crop_location (tuple): Crop location (lower height, upper height, left width, right width).
+        
+    Returns:
+        Tuple containing the expanded location and the expanded image (numpy.ndarray).
+    """
     print("[Console] Salvaging usable cropped portions")
     height, width = get_image_2D_dim(image)
     h_lower, h_upper, w_left, w_right = crop_location
@@ -131,8 +257,15 @@ def expand_from_crop_image(image, crop_location):
         return (None, None)
 
 def remove_black_outline(image):
-    # Crop a threshold mask of the image inward proportionally until all the black pixel are removed
-    # Get the dimension of the mask and crop the original image
+    """
+    Crops the image inward proportionally until all the black pixel outlines are removed.
+    
+    Args:
+        image (numpy.ndarray): Image array to be processed.
+        
+    Returns:
+        Tuple containing the crop location and the cropped image (numpy.ndarray).
+    """
     print("[Console] Cropping Image")
     mask = get_mask_image(image)
     # Cropping image
@@ -146,7 +279,7 @@ def remove_black_outline(image):
             break
     # Showing result
     if is_cropped:
-        print("[Console] Crop successfully")
+        print("[Console] Crop successful")
         return crop_image(image, crop_factor)
     else:
         print("[Console] Image is not suitable to be cropped")
@@ -160,5 +293,9 @@ expand_location, expanded_img = expand_from_crop_image(stitched_image, crop_loca
 # Output
 write_image("./output/stitched_img.jpg", stitched_image)
 show_image("Stitch", stitched_image)
-if cropped_image is not None: write_image("./output/cropped_img.jpg", cropped_image), show_image("Crop", cropped_image)
-if expanded_img is not None: write_image("./output/expanded_img.jpg", expanded_img), show_image("Expand", expanded_img)
+if cropped_image is not None:
+    write_image("./output/cropped_img.jpg", cropped_image)
+    show_image("Crop", cropped_image)
+if expanded_img is not None:
+    write_image("./output/expanded_img.jpg", expanded_img)
+    show_image("Expand", expanded_img)
